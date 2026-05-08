973e7b4ccbb9d2f4773ee4cfec11fe0c4b3565b9
s5027670 <ranvir.arora@student.uq.edu.au>
2026-05-08 11:16:10 +1000

    course planner

--- Code/urls.py
+++ Code/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import dashboard, course_summary
+from .views import dashboard, course_summary, course_planner
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", dashboard, name="dashboard"),
     path("course/<str:course_code>/", course_summary, name="course_summary"),
+    path("planner/", course_planner, name="course_planner"),
 ]
--- Code/views.py
+++ Code/views.py
@@ -6,3 +6,6 @@ def dashboard(request):
 def course_summary(request, course_code=None):
     context = {'course_code': course_code}
     return render(request, "course_summary.html", context)
+
+def course_planner(request):
+    return render(request, "course_planner.html")
