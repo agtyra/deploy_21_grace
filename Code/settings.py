4124c0115e22c8187fc19a3f197d2fcf70851921
Rony-star666 <1216005673@qq.com>
2026-05-11 00:31:07 +1000

    Implement page redirection

--- Code/settings.py
+++ Code/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'Code.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'Code.wsgi.application'
 
 
 # Database
